d2028eba417eba14c7f916d46e099b2a5e421c66
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-03 15:11:27 +0330

    Dbms
    created model data base for blog and website

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    #author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
