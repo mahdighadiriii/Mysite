fa4691955debdbd92d3dcd07d928f5855643b8fa
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-04 14:56:20 +0330

    Tags
    completed html tags structure

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 # Create your views here.
 
@@ -9,13 +9,17 @@ def blog_view(request):
     }
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
-
-
-def test(request):
-    posts = Post.objects.all()
+def blog_single(request,pid):
+    post = get_object_or_404(Post,pk=pid)
     context = {
-        'posts': posts
+        'post': post
+    }
+    return render(request, 'blog/blog-single.html', context)
+
+
+def test(request, pid):
+    post = get_object_or_404(Post,pk=pid)
+    context = {
+        'post': post
     }
     return render(request, 'blog/test.html', context)
