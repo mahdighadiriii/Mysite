70e04d62537bb93ea5ed0e166e95b462abfa4aab
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-05 09:38:34 +0330

    Author

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    #author = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     counted_view = models.IntegerField(default=0)
