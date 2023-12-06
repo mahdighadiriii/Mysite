dc644c238848ff0f6810340d78817166a5ddbd5f
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-06 14:47:34 +0330

    Image Field

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+    image = models.ImageField(upload_to='blog/', default='default.jpg')
     author = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
+    category = models.ManyToManyField('Category')
     content = models.TextField()
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
--- blog/views.py
+++ blog/views.py
@@ -18,9 +18,6 @@ def blog_single(request,pid):
     return render(request, 'blog/blog-single.html', context)
 
 
-def test(request, pid):
-    post = get_object_or_404(Post,pk=pid)
-    context = {
-        'post': post
-    }
-    return render(request, 'blog/test.html', context)
+
+
+
