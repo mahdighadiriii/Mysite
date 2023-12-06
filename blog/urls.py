9c80e7b96ef8bcd045e8033e312155a67cd7cdcd
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-06 17:52:07 +0330

    Paginator Post
    query to give all objects posts and show in the template and added next post and previous post button in template (dynamic)

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single, name='single'),
-    #path('post-<str:pid>',test,name='test'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+
 # Create your views here.
 
 def blog_view(request):
@@ -12,10 +14,23 @@ def blog_view(request):
 def blog_single(request,pid):
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts,pk=pid)
+    single = Post.objects.get(pk=pid)
+    nextpost = Post.objects.filter(id__gt=single.id).order_by('id').first()
+    prevpost = Post.objects.filter(id__lt=single.id).order_by('id').last()
     context = {
-        'post': post
+        'posts': posts,
+        'post': post,
+        'nextpost': nextpost,
+        'prevpost': prevpost,
+        'single':single
     }
     return render(request, 'blog/blog-single.html', context)
+
+
+
+
+
+
 
 
 
