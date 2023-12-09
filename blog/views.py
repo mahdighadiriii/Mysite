2abba3d1687d1aa78d6c9d1fada991d2923a5a18
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-09 14:20:04 +0330

    Template Tags
    added folder and file template tags and development in view and create some function for templates tags and register

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.urls import reverse
 
 # Create your views here.
 
-def blog_view(request):
-    posts = Post.objects.filter(status=1)
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/blog-home.html', context)
+def blog_view(request,**kwargs):
+    posts = Post.objects.filter(status=1).order_by('-published_date')
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
+    if kwargs.get('tag_name') != None:
+        posts = posts.filter(tags__name__in=[kwargs['tag_name']])
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
     posts = Post.objects.filter(status=1)
@@ -24,13 +37,21 @@ def blog_single(request,pid):
         'prevpost': prevpost,
         'single':single
     }
-    return render(request, 'blog/blog-single.html', context)
+    return render (request,'blog/blog-single.html',context)
 
 
-
-
-
-
+def blog_search(request):
+    #print(request.__dict__)
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        #print(request.GET.get('s'))
+        # if s := request.GET.get('s'): #use this method only when your using python version 3.8 and above
+        if request.GET.get('s'):
+            s =  request.GET.get('s')
+            posts = posts.filter(content__contains=s)
+    
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 
 
 
