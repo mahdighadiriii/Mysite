5b2654bbd94e64e981597ffece3e5f52b4d86cf4
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-12-15 20:49:29 +0330

    Sitemaps

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from website.views import *
 
+
+
 app_name = 'website'
+
+
+
+
+
 
 urlpatterns = [
     path('', index_view, name='index'),
@@ -9,4 +16,5 @@ urlpatterns = [
     path('contact/', contact_view, name='contact'),
     path('test/', test_view, name='test'),
     path('newsletter', newsletter_view, name='newsletter'),
+    
 ]
